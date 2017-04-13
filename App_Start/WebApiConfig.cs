c8b6cfb147e9b3352c1754ec3acea595ebbaecdb
dk <dmitry.kharchenko@gmail.com>
2017-04-13 13:26:40 +0300

    upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json;
+
+namespace SqlToRestApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.EnableCors();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller_name}/{parm0}/{parm1}/{parm2}/{parm3}/{parm4}/{min_price}/{max_price}",
+                defaults: new { controller = "values"
+                , parm0 = RouteParameter.Optional 
+                , parm1 = RouteParameter.Optional
+                , parm2 = RouteParameter.Optional
+                , parm3 = RouteParameter.Optional
+                , parm4 = RouteParameter.Optional
+                , min_price = RouteParameter.Optional
+                , max_price = RouteParameter.Optional
+                }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "Help",
+                routeTemplate: "help",
+                defaults: new
+                {  controller = "help" }
+            );
+
+            config.Formatters.Add(new BrowserJsonFormatter());
+        }
+    }
+
+    public class BrowserJsonFormatter : JsonMediaTypeFormatter
+    {
+        public BrowserJsonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            this.SerializerSettings.Formatting = Formatting.Indented;
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json") {CharSet = mediaType.CharSet};
+        }
+    }
+}
---
+++ Services/Config.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+
+namespace SqlToRestApi.Services
+{
+    internal class Config
+    {
+        public static string AppPath
+        {
+            get
+            {
+                var location = Assembly.GetExecutingAssembly().Location;
+                return location != null ? System.IO.Path.GetDirectoryName(location.Trim()) : "";
+            }
+        }
+
+        public static string AppName => Assembly.GetExecutingAssembly().GetName().Name.Trim();
+
+        public static string ApiDbConnectionString { set; get; }
+
+        public static int SqlCommandTimeout { set; get; }
+
+        public static bool AuthorizationRequired { get; set; }
+
+        public static void Read()
+        {
+            ApiDbConnectionString = ConfigurationManager.ConnectionStrings["ApiDbConnection"].ConnectionString;
+            try
+            {
+                SqlCommandTimeout = int.Parse(ConfigurationManager.AppSettings["SqlCommandTimeout"]);
+            }
+            catch (Exception)
+            {
+                SqlCommandTimeout = 900;
+            }
+
+            try
+            {
+                AuthorizationRequired = bool.Parse(ConfigurationManager.AppSettings["AuthorizationRequired"]);
+            }
+            catch (Exception)
+            {
+                AuthorizationRequired = false;
+            }
+        }
+
+        static Config()
+        {
+            Read();
+        }
+    }
+}
